a3c7488a312443c3593a2cc6f57c92db50ed1ba4
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2013-01-30 14:26:40 +0100

    Adding more documentation

--- lib/Catmandu/FedoraCommons/Response.pm
+++ lib/Catmandu/FedoraCommons/Response.pm
@@ -22,14 +22,10 @@ Catmandu::FedoraCommons::Response - Catmandu::FedoraCommons object
 
 A Catmandu::FedoraCommons::Response gets returned for every Catmandu::FedoraCommons method. This response
 contains the raw HTTP content of a Fedora Commons request and can also be used to parse XML responses into
-Perl objects using the parse_content funcion. For more information on the Perl objects see the information
+Perl objects using the parse_content function. For more information on the Perl objects see the information
 in the Catmandu::FedoraCommons::Model packages.
 
-=head1 AUTHORS
-
-=over 4
-
-=item * Patrick Hochstenbach, C<< <patrick.hochstenbach at ugent.be> >>
+=head2 METHODS
 
 =cut
 package Catmandu::FedoraCommons::Response;
@@ -47,18 +43,58 @@ sub factory {
     bless $response , $class;
 }
 
+=head2 is_ok()
+
+Returns true when the result Fedora Commons response contains no errors.
+
+=cut
 sub is_ok {
     my ($self) = @_;
     
     $self->{code} eq '200';
 }
 
+=head2 error()
+
+Returns the error message of the Fedora Commons response if available.
+
+=cut
 sub error {
     my ($self) = @_;
     
     $self->{message};
 }
 
+=head2 parse_content($model)
+
+Returns a Perl representation of the Fedora Commons response. Optionally a  model object can be provided that
+implements a $obj->parse($xml) method and returns a Perl object. If no model is provided then the default 
+models from the Catmandu::FedoraCommons::Model packages are used.
+
+Example:
+
+ package MyParser;
+ 
+ sub new {
+     my $class = shift;
+     return bless {} , $class;
+ }
+ 
+ sub parse {
+     my ($self,$xml) = @_;
+     ...
+     return $perl
+ }
+ 
+ package main;
+ 
+ my $fedora = Catmandu::FedoraCommons->new('http://localhost:8080/fedora','fedoraAdmin','fedoraAdmin');
+
+ my $result = $fedora->findObjects(terms=>'*');
+ 
+ my $perl = $result->parse_content(MyParser->new);
+ 
+=cut
 sub parse_content {
     my ($self,$model) = @_;
     my $method = $self->{method};
@@ -95,28 +131,66 @@ sub parse_content {
     }
 }
 
+=head2 raw()
+
+Returns the raw Fedora Commons response as a string.
+
+=cut
 sub raw {
     my ($self) = @_;
     
     $self->{content};
 }
 
+=head2 content_type()
+
+Returns the Content-Type of the Fedora Commons response.
+
+=cut
 sub content_type {
     my ($self) = @_;
     
     $self->{headers}->{'content-type'}->[0];
 }
 
+=head2 length()
+
+Returns the byte length of the Fedora Commons response.
+
+=cut
 sub length {
     my ($self) = @_;
     
     $self->{headers}->{'content-length'}->[0];
 }
 
+=head2 date()
+
+Returns the date of the Fedora Commons response.
+
+=cut
 sub date {
     my ($self) = @_;
     
     $self->{headers}->{'date'}->[0];
 }
+
+=head1 AUTHORS
+
+=over 4
+
+=item * Patrick Hochstenbach, C<< <patrick.hochstenbach at ugent.be> >>
+
+=back
+
+=head1 SEE ALSO
+
+L<Catmandu::Model::findObjects>,
+L<Catmandu::Model::getObjectHistory>,
+L<Catmandu::Model::getObjectPrifule>,
+L<Catmandu::Model::listDatastreams>,
+L<Catmandu::Model::listMethods>
+
+=cut
 
 1;
