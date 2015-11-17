e0f6191fa331ff6af6da510ecd8ea7f43d61bf51
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2015-11-17 14:42:23 +0100

    0.273

--- lib/Catmandu/FedoraCommons.pm
+++ lib/Catmandu/FedoraCommons.pm
@@ -64,7 +64,7 @@ package Catmandu::FedoraCommons;
 
 use Catmandu::FedoraCommons::Response;
 
-our $VERSION = '0.272';
+our $VERSION = '0.273';
 use URI::Escape;
 use HTTP::Request::Common qw(GET POST DELETE PUT HEAD);
 use LWP::UserAgent;
